008cb8098ca0c664565214c5d0abfd8648ed9a30
Numaan Ahmed <numaanahmed3110@gmail.com>
2024-10-10 18:07:57 +0530

    Added my project

---
+++ src/components/header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <div className="overlay">
+          <div className="video-container">
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              style={{ width: "100%", height: "auto" }}
+            >
+              <source src="\vid\mill-video.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  {props.data ? props.data.title : "Loading"}
+                  <span></span>
+                </h1>
+                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <a
+                  href="#features"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Learn More
+                </a>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
